80f0b3cecaedca8ae741bffa32ce8181afaeaaf1
abdul waqar <awaqar143@gmail.com>
2020-08-08 18:50:46 +0500

    make it better

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,7 @@ class Home extends CI_Controller
 
 		public function __construct()
 	{
-		parent::__construct();
+		parent :: __construct();
 		$this->load->helper('url');
 		$this->load->helper('form');
 		$this->load->library('form_validation');
